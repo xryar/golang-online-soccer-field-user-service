7ddaab2a1c3c12dfda49a1dadee994cde0ccf3d7
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-15 15:39:11 +0700

    Implement database initialization function with connection pooling settings

---
+++ config/database.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"net/url"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDatabase() (*gorm.DB, error) {
+	config := Config
+	encodedPassword := url.QueryEscape(config.Database.Password)
+	uri := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable",
+		config.Database.Name,
+		encodedPassword,
+		config.Database.Host,
+		config.Database.Port,
+		config.Database.Name,
+	)
+
+	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxIdleConns(config.Database.MaxIdleConnection)
+	sqlDB.SetMaxOpenConns(config.Database.MaxOpenConnection)
+	sqlDB.SetConnMaxLifetime(time.Duration(config.Database.MaxLifetimeConnection) * time.Second)
+	sqlDB.SetConnMaxIdleTime(time.Duration(config.Database.MaxIdleTime) * time.Second)
+	return db, nil
+}
