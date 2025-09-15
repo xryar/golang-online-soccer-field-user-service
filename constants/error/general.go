2e565cda2b5a027917c4a46bb6528ce49af8225c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-15 13:38:36 +0700

    Refactor error handling: remove unused constants and implement HttpResponse function for standardized API responses

--- constants/error/general.go
+++ constants/error/general.go
@@ -2,11 +2,6 @@ package error
 
 import "errors"
 
-const (
-	Success = "success"
-	Error   = "error"
-)
-
 var (
 	ErrInternalServerError = errors.New("internal server error")
 	ErrSQLError            = errors.New("database server failed to execute query")
