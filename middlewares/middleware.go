15736c0c0edc76b0dc7e9e7676ea153cb5b4f122
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-17 09:38:41 +0700

    Add middleware for panic handling and rate limiting

---
+++ middlewares/middleware.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import (
+	"net/http"
+	"user-service/common/response"
+	"user-service/constants"
+	errConstant "user-service/constants/error"
+
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func HandlePanic() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		defer func() {
+			if r := recover(); r != nil {
+				logrus.Errorf("Recovered from panic: %v", r)
+				ctx.JSON(http.StatusInternalServerError, response.Response{
+					Status:  constants.Error,
+					Message: errConstant.ErrInternalServerError.Error(),
+				})
+				ctx.Abort()
+			}
+		}()
+		ctx.Next()
+	}
+}
+
+func RateLimiter(lmt *limiter.Limiter) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		err := tollbooth.LimitByRequest(lmt, ctx.Writer, ctx.Request)
+		if err != nil {
+			ctx.JSON(http.StatusTooManyRequests, response.Response{
+				Status:  constants.Error,
+				Message: errConstant.ErrTooManyRequest.Error(),
+			})
+			ctx.Abort()
+		}
+		ctx.Next()
+	}
+}
