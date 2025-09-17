703978b0485f7bb12516d7b6b5506f1f0e6196bd
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-17 15:24:09 +0700

    Implement server initialization with routing, middleware, and rate limiting

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,23 @@
-package main
+package cmd
 
 import (
+	"fmt"
+	"net/http"
 	"time"
+	"user-service/common/response"
 	"user-service/config"
+	"user-service/constants"
 	"user-service/controllers"
 	"user-service/database/seeders"
 	"user-service/domain/models"
+	"user-service/middlewares"
 	"user-service/repositories"
+	"user-service/routes"
 	"user-service/services"
 
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
@@ -42,5 +51,49 @@ var command = &cobra.Command{
 		repository := repositories.NewRegistryRepository(db)
 		service := services.NewRegistryService(repository)
 		controller := controllers.NewRegistryController(service)
+
+		router := gin.Default()
+		router.Use(middlewares.HandlePanic())
+		router.NoRoute(func(ctx *gin.Context) {
+			ctx.JSON(http.StatusNotFound, response.Response{
+				Status:  constants.Error,
+				Message: fmt.Sprintf("Path %s", http.StatusText(http.StatusNotFound)),
+			})
+		})
+		router.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, response.Response{
+				Status:  constants.Success,
+				Message: "Welcome to User Service",
+			})
+		})
+		router.Use(func(ctx *gin.Context) {
+			ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT")
+			ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-service-name, x-api-key, x-request-at")
+			ctx.Next()
+		})
+
+		lmt := tollbooth.NewLimiter(
+			config.Config.RateLimiterMaxRequest,
+			&limiter.ExpirableOptions{
+				DefaultExpirationTTL: time.Duration(config.Config.RateLimiterTimeSecond) * time.Second,
+			},
+		)
+
+		router.Use(middlewares.RateLimiter(lmt))
+
+		group := router.Group("/api/v1")
+		route := routes.NewRegistryRoute(controller, group)
+		route.Serve()
+
+		port := fmt.Sprintf(":%d", config.Config.Port)
+		router.Run(port)
 	},
+}
+
+func Run() {
+	err := command.Execute()
+	if err != nil {
+		panic(err)
+	}
 }
